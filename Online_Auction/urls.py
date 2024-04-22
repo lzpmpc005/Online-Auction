129c2bf47fe7d4a8246edc7c7a2e64a321b2fb73
vincentyelpen <vincent.yelpen@outlook.com>
2024-04-22 23:36:50 +0200

    frontend and api

--- Online_Auction/urls.py
+++ Online_Auction/urls.py
@@ -2,13 +2,21 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from auctions.views import ItemViewSet, BidViewSet
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 
 router = DefaultRouter()
 router.register(r'items', ItemViewSet)
 router.register(r'bids', BidViewSet)
 
+# API的URL模式
+api_urlpatterns = [
+    path('', include(router.urls)),
+
+]
+
+# 其他URL模式（如admin等）
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api/', include(api_urlpatterns)),
+
 ]
